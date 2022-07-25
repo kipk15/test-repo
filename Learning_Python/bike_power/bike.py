c4e06aad870376768b2ac50305d161bc8f692f47
ron <kipkoechronald5@gmail.com>
2022-07-25 00:53:00 -0400

    Update bike.py

--- Learning_Python/bike_power/bike.py
+++ Learning_Python/bike_power/bike.py
@@ -11,28 +11,39 @@ key1 = False
 def is_ignited(key):
     #prep data for reading
     if key:
-        batteries1 = [["a", "full"],["b", "empty"]]
-        switch1 = ("a", "none", "b")
+        batteries = [["a", "full"],["b", "empty"]]
+        switch= ("a", "none", "b")
+        switch_a = switch[0]
+        switch_none = switch[1]
+        switch_b = switch[2]
+        battery_a = batteries[0]
+        battery_b = batteries[1]
+        print(f"{battery_a} is {battery_a[1]}")
+        print(f"{battery_b} is {battery_b[1]}")
+        
+
         # fuction1: is_battery
         # switches (a, b, c)
         # checks battery power status, switch selection
         # returns True of False
-        def is_battery(batteries, switch):
-            switch_status = switch[0]
-            i = 0
-            while i < len(batteries):
-                battery_name = batteries[i][0]
-                battery_status = batteries[i][1]
-                if (switch_status == battery_name) and (battery_status == "full"):
-                    print(f"we have power in {battery_name}, ready to ignite")
-                if (switch_status == battery_name) and (battery_status == "empty"):
-                    print(f"we do not have power in {battery_name}")
-                if (switch_status != battery_name) and (battery_status == "full"):
-                    print(f"The switch is not on, press {battery_name} to use battery")
-                if (switch_status != battery_name) and (battery_status == "empty"):
-                    print(f"The switch is not on, and neither does the battery have power")
-                i = i + 1
-        is_battery(batteries1, switch1)
+        def is_battery(battery, switch):
+            battery_name = battery[0]
+            battery_status = battery[1]
+            if (switch == battery_name) and (battery_status == "full"):
+                print(f"Switch is on, and battery '{battery_name}' is charged --> Ignition Ready!")
+            elif (switch == battery_name) and (battery_status == "empty"):
+                print(f"Switch is on, but battery '{battery_name}' isn't charged")
+            elif (switch != battery_name) and (battery_status == "full"):
+                print(f"Switch not on, but battery '{battery_name}' is charged")
+            elif (switch != battery_name) and (battery_status == "empty"):
+                print(f"Switch not on, and neither is battery '{battery_name}' charged")
+        is_battery(battery_a, switch_a)
+        is_battery(battery_a, switch_none)
+        is_battery(battery_a, switch_b)
+        is_battery(battery_b, switch_b)
+        is_battery(battery_b, switch_a)
+        is_battery(battery_b, switch_none)
+
 is_ignited(key0)
 
     # checks if battery power reaches
