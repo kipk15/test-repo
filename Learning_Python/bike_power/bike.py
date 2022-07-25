4536a618bdbc37413a5c0249f17d99a9ed797afb
ron <kipkoechronald5@gmail.com>
2022-07-24 23:47:58 -0400

    Update bike.py
    Added if-statements to is_battery function

--- Learning_Python/bike_power/bike.py
+++ Learning_Python/bike_power/bike.py
@@ -2,24 +2,38 @@
 # that illustrates the flow of power
 # throughout the bike(i.e battery, controller, throtle, motor)
 
-batteries1 = [["a", "full"]["b", "empty"]]
-switch1 = ("a", "none", "b")
-# ignition
-# checks if bike is on
-# returns True or False
-def is_ignited():
-    # switches (a, b, c)
-    # checks battery power status, switch selection
-    # returns True of False
-    def is_battery(batteries, switch):
-        i = 0
-        while i < len(batteries):
-            if batteries[i][1] == "full" and switch == batteries[i][0]:
-                print(f"we have power in {batteries[i][0]}")
+
+# fuction0: is_ignited
+# checks if bike is i.e
+# if key is inserted or not
+key0 = True
+key1 = False
+def is_ignited(key):
+    #prep data for reading
+    if key:
+        batteries1 = [["a", "full"],["b", "empty"]]
+        switch1 = ("a", "none", "b")
+        # fuction1: is_battery
+        # switches (a, b, c)
+        # checks battery power status, switch selection
+        # returns True of False
+        def is_battery(batteries, switch):
+            switch_status = switch[0]
+            i = 0
+            while i < len(batteries):
+                battery_name = batteries[i][0]
+                battery_status = batteries[i][1]
+                if (switch_status == battery_name) and (battery_status == "full"):
+                    print(f"we have power in {battery_name}, ready to ignite")
+                if (switch_status == battery_name) and (battery_status == "empty"):
+                    print(f"we do not have power in {battery_name}")
+                if (switch_status != battery_name) and (battery_status == "full"):
+                    print(f"The switch is not on, press {battery_name} to use battery")
+                if (switch_status != battery_name) and (battery_status == "empty"):
+                    print(f"The switch is not on, and neither does the battery have power")
                 i = i + 1
-                return True
-    is_battery(batteries1[], switch1)
-is_ignited()
+        is_battery(batteries1, switch1)
+is_ignited(key0)
 
     # checks if battery power reaches
     # the controller
