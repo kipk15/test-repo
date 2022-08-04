3c04fa3a9a125f0b187858d6f3b341fe96899251
ron <kipkoechronald5@gmail.com>
2022-08-04 00:22:10 -0400

    Update lottery.py

--- Learning_Python/Tools_Set/Classes/lottery.py
+++ Learning_Python/Tools_Set/Classes/lottery.py
@@ -1,11 +1,14 @@
 from random import randint
 
+
 # randomly pick an index, and display value at that index
 pocket = [34, 14, 'w', 8, 1, 6, 'y', 33, 0, 3, 'd', 4, 'e', 7,'f']
-m = 0
-print("These are the lucky tickets: \n")
-while m < 4:
+my_ticket = 34
+#print("These are the lucky tickets: \n")
+i = randint(0, (len(pocket)-1))
+count = 0
+while my_ticket != pocket[i]:
+    count += 1
     i = randint(0, (len(pocket)-1))
-    print (i)
-    print(f"\t{pocket[i]}")
-    m +=1
+print(f"Your ticket number, {my_ticket}, emerged a winner after {count} rolls")
+
