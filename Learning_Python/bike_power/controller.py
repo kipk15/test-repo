6fd6d8a358340c9f004bd3fe0975ccc41e959598
ron <kipkoechronald5@gmail.com>
2022-08-05 01:07:17 -0400

    controller class
    a class that provided specifications of a given controller, neatly displays them

--- Learning_Python/bike_power/controller.py
+++ Learning_Python/bike_power/controller.py
@@ -1,4 +1,17 @@
-# checks if battery power reaches
-    # the controller
-    # returns True or False
-    #def is_controller()
+class Controller():
+    def __init__(self, model, volts, oc_protection, dom):
+        self.volts = volts
+        self.oc_protection = oc_protection
+        self.dom = dom
+        self.model = model
+
+    def controller_info(self):
+        controller_specs = (
+                f"\n\tModel: {self.model}"
+                f"\n\tWorking Voltage: {self.volts} Volts"
+                f"\n\tOver-Current Protection: {self.oc_protection} Ah"
+                f"\n\tDate of Manufacture: {self.dom}")
+        return (controller_specs)
+
+controller_0 = Controller('DHFKW3849WS', 60, 15, '04/05/2022')
+print(f"{controller_0.controller_info()}")
