7bf7de8389113ffaa3135b234797fe083d407f82
ron <kipkoechronald5@gmail.com>
2022-08-06 12:13:47 -0400

    Update battery.py
    using classes and property decorators

--- Learning_Python/bike_power/battery.py
+++ Learning_Python/bike_power/battery.py
@@ -1,42 +1,33 @@
-# fuction0: is_ignited
-# checks if bike is i.e
-# if key is inserted or not
-key0 = True
-key1 = False
-def is_ignited(key):
-    #prep data for reading
-    if key:
-        batteries = [["a", "full"],["b", "empty"]]
-        switch= ("a", "none", "b")
-        switch_a = switch[0]
-        switch_none = switch[1]
-        switch_b = switch[2]
-        battery_a = batteries[0]
-        battery_b = batteries[1]
-        print(f"{battery_a} is {battery_a[1]}")
-        print(f"{battery_b} is {battery_b[1]}")
+# battery class
+
+class Battery():
+    def __init__(self, voltage, switch = 'on',status = 'charged'):
+        self.voltage = voltage
+        self.status = status
+        self.switch = switch
+
+    @property
+    def is_on(self):
+        if self.switch == 'on':
+            return True
+        return False
+
+    @property
+    def is_charged(self):
+        if self.status == 'charged':
+            return True
+        return False
+
+    @property
+    def capacity(self):
+        return self.voltage
+
+    def running(self):
+        if self.is_on and self.is_charged:
+            return True
+        return False
         
-
-        # fuction1: is_battery
-        # switches (a, b, c)
-        # checks battery power status, switch selection
-        # returns True of False
-        def is_battery(battery, switch):
-            battery_name = battery[0]
-            battery_status = battery[1]
-            if (switch == battery_name) and (battery_status == "full"):
-                print(f"Switch is on, and battery '{battery_name}' is charged --> Ignition Ready!")
-            elif (switch == battery_name) and (battery_status == "empty"):
-                print(f"Switch is on, but battery '{battery_name}' isn't charged")
-            elif (switch != battery_name) and (battery_status == "full"):
-                print(f"Switch not on, but battery '{battery_name}' is charged")
-            elif (switch != battery_name) and (battery_status == "empty"):
-                print(f"Switch not on, and neither is battery '{battery_name}' charged")
-        is_battery(battery_a, switch_a)
-        is_battery(battery_a, switch_none)
-        is_battery(battery_a, switch_b)
-        is_battery(battery_b, switch_b)
-        is_battery(battery_b, switch_a)
-        is_battery(battery_b, switch_none)
-
-is_ignited(key0)
+battery_a = Battery(60, 'off', 'charged')
+battery_b = Battery(60, 'on', 'empty')
+print(battery_a.running())
+print(battery_b.running())
--- Learning_Python/bike_power/controller.py
+++ Learning_Python/bike_power/controller.py
@@ -1,10 +1,14 @@
 class Controller():
+    # class variables
+    
+    #instance variables
     def __init__(self, model, volts, oc_protection, dom):
-        self.volts = volts
+        self.self.volts = volts
         self.oc_protection = oc_protection
         self.dom = dom
         self.model = model
 
+    @property # access this method as an attribute
     def controller_info(self):
         controller_specs = (
                 f"\n\tModel: {self.model}"
@@ -14,4 +18,4 @@ class Controller():
         return (controller_specs)
 
 controller_0 = Controller('DHFKW3849WS', 60, 15, '04/05/2022')
-print(f"{controller_0.controller_info()}")
+print(f"{controller_0.controller_info}")
